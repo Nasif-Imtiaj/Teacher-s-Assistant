18bac69c464dae7d909f2a08d7cb7094f1e77038
nasif <nasif@inverseai.com>
2022-08-10 18:17:31 +0600

    Implement to upload a file to firebase.

--- app/src/main/java/com/ni/ui/screens/library/LibraryFragment.kt
+++ app/src/main/java/com/ni/ui/screens/library/LibraryFragment.kt
@@ -1,5 +1,13 @@
 package com.ni.ui.screens.library
+
+import android.R
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import com.ni.data.models.Booklet
 import com.ni.teachersassistant.databinding.BookletItemLayoutBinding
 import com.ni.teachersassistant.databinding.LibraryFragmentLayoutBinding
@@ -13,13 +21,14 @@ class LibraryFragment : BaseObservableFragment<LibraryFragmentLayoutBinding, Lib
         const val TAG = "LibraryFragment"
         fun newInstance() = LibraryFragment().apply {}
     }
+    lateinit var storage: FirebaseStorage
     private val bookletListAdapter: AbstractAdapter<Booklet, BookletItemLayoutBinding> by lazy {
         object :
             AbstractAdapter<Booklet, BookletItemLayoutBinding>(BookletItemLayoutBinding::inflate) {
             override fun bind(
                 itemBinding: BookletItemLayoutBinding,
                 item: Booklet,
-                position: Int
+                position: Int,
             ) {
                 itemBinding.tvName.text = item.name
             }
@@ -35,10 +44,29 @@ class LibraryFragment : BaseObservableFragment<LibraryFragmentLayoutBinding, Lib
     private fun initUiListener() {
         initBtnListener()
         initRecycler()
+        storage = Firebase.storage
     }
 
     private fun initBtnListener() {
-        binding.bUpload.setOnClickListener {}
+        binding.bUpload.setOnClickListener {
+            Log.d(TAG, "initBtnListener: UploadFile")
+             uploadFile()
+            Toast.makeText(activity,"UploadClicked",Toast.LENGTH_SHORT)
+        }
+    }
+
+    private fun uploadFile(){
+        var fileName = "testName"
+        var storageRef = storage.reference.child("library/").child(fileName)
+        val url = Uri.parse("android.resource://" + activity?.packageName.toString() + "/" + R.drawable.ic_delete)
+        var uploadTask = storageRef.putFile(url)
+
+        uploadTask.addOnFailureListener {
+         Toast.makeText(activity,"Failed",Toast.LENGTH_SHORT)
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
     }
 
     private fun initRecycler() {
